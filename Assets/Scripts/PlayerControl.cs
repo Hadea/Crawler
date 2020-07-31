95221734d26da210df23e58a5b8412218589194b
Lutz <lambda2@gmx.at>
2020-07-31 15:05:03 +0200

    Added Player Controls

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float MovementSpeed;
+
+
+    void Update()
+    {
+        Vector3 movement = new Vector3();
+        movement.x = MovementSpeed * Input.GetAxis("Horizontal");
+        movement.z = MovementSpeed * Input.GetAxis("Vertical");
+
+        transform.Translate(movement * Time.deltaTime, Space.World);
+    }
+}
