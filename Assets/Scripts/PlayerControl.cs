3bf530e2d4803259719df4b3d23084eeadd795bb
Lutz <lambda2@gmx.at>
2020-07-31 15:21:14 +0200

    Added Player Rotation

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -10,10 +10,29 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
+
+        #region Movement
+
         Vector3 movement = new Vector3();
         movement.x = MovementSpeed * Input.GetAxis("Horizontal");
         movement.z = MovementSpeed * Input.GetAxis("Vertical");
 
         transform.Translate(movement * Time.deltaTime, Space.World);
+        #endregion
+
+        #region Rotation
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+        float hit;
+        Vector3 target = new Vector3();
+        if (plane.Raycast(ray, out hit))
+        {
+             target = ray.origin + ray.direction * hit;
+        }
+
+        transform.LookAt(target);
+        #endregion
     }
 }
--- Assets/Scripts/__MouseControl.cs
+++ Assets/Scripts/__MouseControl.cs
@@ -13,6 +13,6 @@ public class __MouseControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
