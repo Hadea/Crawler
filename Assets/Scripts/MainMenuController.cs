192b5f3e8b1ec916f15eee6bf989e1a4e250feac
Lutz <lambda2@gmx.at>
2020-08-01 10:25:49 +0200

    Added main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnButtonExitClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnButtonNewGameClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
